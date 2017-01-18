504a3f4403a07ce34a6500046b9c4f2fe88d6bbf
wizar <wizar92@gmail.com>
2017-01-18 21:14:34 +0500

    Gulp
    Sourcemaps, concat, uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sourcemaps = require('gulp-sourcemaps'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
 
 gulp.task('js:build', function () {
 	gulp.src('src/js/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(concat('app.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build'));
+
+	// Don't do anythings with background files, just copy
+	gulp.src('src/js/background/*.js')
 		.pipe(gulp.dest('build'));
 });
 
