567aa3919f1c064bffbf7c5605bc89c421a67892
wizar <wizar92@gmail.com>
2017-01-24 00:20:08 +0500

    Prebuild DB

--- src/js/db-helper.js
+++ src/js/db-helper.js
@@ -1,8 +1,29 @@
 import PouchDB from 'pouchdb';
+import PouchDBLoad from 'pouchdb-load';
+
+PouchDB.plugin(PouchDBLoad);
 
 class DBHelper {
 	constructor(pouchDB) {
 		this.db = pouchDB || new PouchDB('time-hero-db');
+	}
+
+	loadDefault(path = './time-hero.txt') {
+		var db = this.db;
+		return db.get('_local/preloaded').then(function(doc) {
+			}).catch(function(err) {
+				if (err.name !== 'not_found') {
+					throw err;
+				}
+
+				return db.load(path).then(function() {
+					return db.put({_id: '_local/preloaded'});
+				});
+			});
+	}
+
+	getById(id) {
+		return this.db.get(id);
 	}
 }
 
