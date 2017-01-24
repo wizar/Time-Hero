f58332c963018735bc16856af0ca7b9f9aca2320
wizar <wizar92@gmail.com>
2017-01-25 00:38:34 +0500

    Reinventig tests
    Unlimited TestWorks has begun
    Using electron-mocha instead mocha

--- test/db-test.js
+++ test/db-test.js
@@ -1,13 +1,21 @@
 import PouchDB from 'pouchdb';
 import memdown from 'memdown';
-import DBHelper from '../src/js/db-helper';
+import {dbHelper} from '../src/js/db-helper';
 import assert from 'assert';
 
 describe('Database helper', function() {
 
 	it('initialize empty db', function() {
-		var inMemoryDB = new PouchDB('test', {db: memdown});
-		var dbHelper = new DBHelper(inMemoryDB);
+		// var inMemoryDB = new PouchDB('test', {db: memdown});
+		// dbHelper = new DBHelper(inMemoryDB);
+	});
+
+	it('load prebuilt DB', function() {
+		dbHelper.loadDefault('other/time-hero.txt').then(function() {
+			return dbHelper.getById('preferences');
+		}).then(function(res) {
+			assert.equal(res.username, 'Best1 Hero')
+		});
 	});
 
 });
